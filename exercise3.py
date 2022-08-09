a1d7a2f2412e7b7cfc436e7678ba6e844e62d0d4
MGonza20 <pag20634@uvg.edu.gt>
2022-08-08 18:08:16 -0600

    probando nuevo formato bmp (24 bits)

--- exercise3.py
+++ exercise3.py
@@ -4,20 +4,20 @@ from shaders import fragmentShader, flat, glow, textureShader, flatNtex
 from texture import Texture
 import random
 
-width = 100
-height = 100
+width = 500
+height = 500
 
 rend = Renderer(width, height)
 
 rend.active_shader = flatNtex
 
-rend.active_texture = Texture("./models/modelTexture.bmp")
+rend.active_texture = Texture("./models/cola_tex.bmp")
 
 
 rend.glLoadModel("./models/cola_can.obj", 
                  translate= V3(width/2, height/2, 0), 
                  rotate = V3(0, 0, 0),
-                 scale= V3(4.5, 4.5, 4.5))
+                 scale= V3(5.4, 5.4, 5.4))
 
 rend.glFinish("outputs/pirate_coin.bmp")
 
